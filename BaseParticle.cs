3ce831e7931a544b8aac62bc726f4631080117ad
RaphaelaHeil <rheil@stud.hs-bremen.de>
2015-11-26 21:30:59 +0100

    added opentk

---
+++ BaseParticle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystems
+{
+
+    using OpenTK.Graphics.OpenGL4;
+
+    abstract class BaseParticle
+    {
+
+        private int maxAge;
+        private int remainingLifetime;
+        private int agingSpeed;
+
+
+        /// <summary>
+        /// Initialises a new Particle with the given maximum age and aging speed.
+        /// </summary>
+        /// <param name="maxAge">The maximum age a particle can reach before being removed from the system.</param>
+        /// <param name="agingSpeed">The speed at which the particle ages.</param>
+        public BaseParticle(int maxAge, int agingSpeed)
+        {
+            this.maxAge = maxAge;
+            this.remainingLifetime = maxAge;
+            this.agingSpeed = agingSpeed;
+        }
+        
+        /// <summary>
+        /// TODO 
+        /// </summary>
+        /// <param name="speed"></param>
+        public void setAgingSpeed(int speed)
+        {
+            if (speed > 0)
+            {
+                agingSpeed = speed;
+            }
+        }
+
+        
+        /// <summary>
+        /// TODO 
+        /// </summary>
+        /// <param name="maxAge"></param>
+        public void setMaxAge(int maxAge)
+        {
+            if (maxAge > 0)
+            {
+                this.maxAge = maxAge;
+                this.remainingLifetime = maxAge;
+            }
+        }
+        
+        
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ParticleSystems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.GLControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.amountBox = new System.Windows.Forms.TextBox();
+            this.renderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.Location = new System.Drawing.Point(12, 12);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(478, 438);
+            this.glControl.TabIndex = 0;
+            this.glControl.VSync = false;
+            this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of particles:";
+            // 
+            // amountBox
+            // 
+            this.amountBox.Location = new System.Drawing.Point(630, 44);
+            this.amountBox.Name = "amountBox";
+            this.amountBox.Size = new System.Drawing.Size(100, 20);
+            this.amountBox.TabIndex = 2;
+            // 
+            // renderButton
+            // 
+            this.renderButton.Location = new System.Drawing.Point(655, 88);
+            this.renderButton.Name = "renderButton";
+            this.renderButton.Size = new System.Drawing.Size(75, 23);
+            this.renderButton.TabIndex = 3;
+            this.renderButton.Text = "Render";
+            this.renderButton.UseVisualStyleBackColor = true;
+            this.renderButton.Click += new System.EventHandler(this.renderButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.Controls.Add(this.renderButton);
+            this.Controls.Add(this.amountBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox amountBox;
+        private System.Windows.Forms.Button renderButton;
+    }
+}
+
