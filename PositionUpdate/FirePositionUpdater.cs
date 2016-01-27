d45445925f3bb4b4aff5b9f2cfb5207ec863e23c
Timo Wilken <wilken.timo@gmail.com>
2016-01-27 22:29:05 +0100

    Comments and Coequality improvements

--- PositionUpdate/FirePositionUpdater.cs
+++ PositionUpdate/FirePositionUpdater.cs
@@ -4,6 +4,7 @@ using OpenTK;
 using ParticleSystems.Particles;
 using ParticleSystems;
 using ParticleSystems.SettingsPanels;
+
 namespace ParticleSystems.PositionUpdate
 {
 
@@ -17,73 +18,60 @@ namespace ParticleSystems.PositionUpdate
 		private Vector2d TranslationX;
 		private Vector2d TranslationYup;
 		private Vector2d TranslationYdown;
-		LifetimeHandler LifetimeHandler = new LifetimeHandler();
-		ExpirationHandler ExpirationHandler = new ExpirationHandler();
+		LifetimeHandler LifetimeHandler = new LifetimeHandler ();
+		ExpirationHandler ExpirationHandler = new ExpirationHandler ();
 
 
-		public FirePositionUpdater()
+		public FirePositionUpdater ()
 		{
-			TranslationX = new Vector2d(DEFAULT_DELTA, 0);
-			TranslationYup = new Vector2d(0, DEFAULT_DELTA);
-			TranslationYdown = new Vector2d(0, -DEFAULT_DELTA);
+			TranslationX = new Vector2d (DEFAULT_DELTA, 0);
+			TranslationYup = new Vector2d (0, DEFAULT_DELTA);
+			TranslationYdown = new Vector2d (0, -DEFAULT_DELTA);
 		}
 
 		/// <see cref="PositionUpdater.UpdatePositions(List{Particle})"/>
-		public void UpdatePositions(List<Particle> particles)
+		public void UpdatePositions (List<Particle> particles)
 		{
 			bool isFire = true;
-			//List<PlaceableObject> placableObjectList = context.getPlacableObjectList();
 			foreach (var particle in particles) {
-				int particlePosX = (int)particle.GetPosition().X;
-				int particlePosY = (int)particle.GetPosition().Y;
+				int particlePosX = (int)particle.GetPosition ().X;
+				int particlePosY = (int)particle.GetPosition ().Y;
 				if (isFire) {
 					Random rand = new Random ();
 					double x = rand.NextDouble ();
-
-					if (x > 0.5) 
-					{
+					// To generate movings of the particles
+					if (x > 0.5) {
 						x = x - 1;
 					}
 					x = x / 5;
 
 					Vector2d Translation = new Vector2d (x, DEFAULT_DELTA);
 					particle.updatePosition (Translation);
-					/**for (int i = 0; i < particles.Count; i++) {
-						if (particlePosX == (particles[i].GetPosition().X) || particlePosY == (particles[i].GetPosition().Y )) {
-							LifetimeHandler.DecrementLifetime (particles);
-							ExpirationHandler.handleExpiration (particles);
-						}
-					}**/
-
-
-
 					isFire = false;
-				} else 
-				{
-					Random rand = new Random();
-					double x = rand.NextDouble();
-
-					if (x > 0.5)
-					{
+				} else {
+					Random rand = new Random ();
+					double x = rand.NextDouble ();
+					if (x > 0.5) {
 						x = x - 1;
 					}
 					x = x / 5;
 
-					Vector2d Translation = new Vector2d(x, -DEFAULT_DELTA);
-					particle.updatePosition(Translation);
+					Vector2d Translation = new Vector2d (x, -DEFAULT_DELTA);
+					particle.updatePosition (Translation);
 					isFire = true;
 				}
-
 			}
 		}
 
 
-		public void SetContext(Context context) {
+		public void SetContext (Context context)
+		{
 			this.context = context;
 		}
 
-		public void SetSettingsPanel(ParticleSystemSettingsPanel settingsPanel){
-		//TODO
+		public void SetSettingsPanel (ParticleSystemSettingsPanel settingsPanel)
+		{
+			//TODO
 		}
 	}
 }
--- Systems/FireParticleSystem.cs
+++ Systems/FireParticleSystem.cs
@@ -6,200 +6,152 @@ using ParticleSystems.PositionUpdate;
 using ParticleSystems.ParticleGeneration;
 using ParticleSystems.SettingsPanels;
 
-namespace ParticleSystems.Systems {
-	class FireParticleSystem : ParticleSystem {
-		private PositionUpdater PositionUpdater = new FirePositionUpdater();
-		private LifetimeHandler LifetimeHandler = new LifetimeHandler();
-		private ExpirationHandler ExpirationHandler = new ExpirationHandler();
-		private Random Rand = new Random();
+namespace ParticleSystems.Systems
+{
+	class FireParticleSystem : ParticleSystem
+	{
+		private PositionUpdater PositionUpdater = new FirePositionUpdater ();
+		private LifetimeHandler LifetimeHandler = new LifetimeHandler ();
+		private ExpirationHandler ExpirationHandler = new ExpirationHandler ();
 
-		private FireUserSettings Panel = new FireUserSettings();
+		private FireUserSettings Panel = new FireUserSettings ();
 
 		private FireParticleGenerator ParticleGenerator;
 
 		private int minX = 280;
 		private int maxX = 320;
-        private int einflussbereich = 5;
+		private int range = 5;
 
-		public FireParticleSystem() {
+		public FireParticleSystem ()
+		{
 
-			//Panel = new AirFlowUserSettings();
 		}
 
-		protected override void Initialise() {
-			ParticleGenerator = new FireParticleGenerator(
-				Context.GetIdHolder().Width,
-				Context.GetIdHolder().Height,
-				ParticleSettings.GetLifetime(),
-				ParticleSettings.GetAgingVelocity(),
-				ParticleSettings.GetVelocity()
+		protected override void Initialise ()
+		{
+			ParticleGenerator = new FireParticleGenerator (
+				Context.GetIdHolder ().Width,
+				Context.GetIdHolder ().Height,
+				ParticleSettings.GetLifetime (),
+				ParticleSettings.GetAgingVelocity (),
+				ParticleSettings.GetVelocity ()
 			);
 
-            minX = Panel.GetXMin();
-            maxX = Panel.GetXMax();
-            einflussbereich = Panel.GetRange();
+			minX = Panel.GetXMin ();
+			maxX = Panel.GetXMax ();
+			range = Panel.GetRange ();
 
-			CreateInitialParticles();
+			CreateInitialParticles ();
 
 			//TODO: create stuff from settings
 			//TODO: generate initial particles
 		}
 
-		protected override void UpdateVBOs() {
+		protected override void UpdateVBOs ()
+		{
 			bool gelb = true;
 			ParticlePositions = new Vector2d[Particles.Count]; 
 			ParticleColours = new Vector3d[Particles.Count];
 
-			for (int i = 0; i < Particles.Count-1; i+=2) {
+			// To check if the fire particles touch the warter particles
+			for (int i = 0; i < Particles.Count - 1; i += 2) {
 				Vector2d fire = Particles.ElementAt (i).GetPosition ();
-				if(fire.X <=maxX+einflussbereich && fire.X >= minX-einflussbereich){
-					for (int x = 0; x < Particles.Count-1; x+=2) {
-						Vector2d water = Particles.ElementAt (x+1).GetPosition ();
-						if (Math.Abs((int)fire.X-(int)water.X) <= einflussbereich && Math.Abs((int)fire.Y-(int)water.Y) <= einflussbereich) {
-
-							//	System.Diagnostics.Debug.WriteLine ("Position: " + Particles.ElementAt(i).GetPosition()+ ", Position i+1: " + Particles.ElementAt(i+1).GetPosition());
+				if (fire.X <= maxX + range && fire.X >= minX - range) {
+					for (int x = 0; x < Particles.Count - 1; x += 2) {
+						Vector2d water = Particles.ElementAt (x + 1).GetPosition ();
+						if (Math.Abs ((int)fire.X - (int)water.X) <= range && Math.Abs ((int)fire.Y - (int)water.Y) <= range) {
 
 							Particles.ElementAt (i).SetExpired ();
 							Particles.ElementAt (i + 1).SetExpired ();
 							break;
 						}
-
+					}
 				}
-
-			
-
-				}
-
 			}
 
-			int removed = ExpirationHandler.handleExpiration (Particles);
 			for (int i = 0; i < Particles.Count; i++) {
 				
-				ParticlePositions[i] = Particles.ElementAt(i).GetPosition();
-
+				ParticlePositions [i] = Particles.ElementAt (i).GetPosition ();
 				double yellow = 1;
 
-				if (gelb == true ) {
-					yellow = (yellow - (( Particles.ElementAt(i).GetPosition().Y) /120 ));
+				if (gelb == true) {
+					yellow = (yellow - ((Particles.ElementAt (i).GetPosition ().Y) / 120));
 					Vector3d vec3color = new Vector3d (1, yellow, 0);
-					ParticleColours[i] = vec3color;
-
-					//LifetimeHandler.DecrementLifetime (Particles);
-					//ExpirationHandler.handleExpiration (Particles);
-
-
-						//ParticlePositions [x];
-						//for (int y = 0; y < Particles.Count; y++) {
-								
-
-
-
+					ParticleColours [i] = vec3color;
 					gelb = false;
-				}   else {
-					//blue = (blue - ((Particles.ElementAt(i).GetPosition().Y)/120));
+				} else {
 					Vector3d vec3colorWater = new Vector3d (0, 0, 255);
-					ParticleColours[i] = vec3colorWater;
-
-
-
+					ParticleColours [i] = vec3colorWater;
 					gelb = true;
 				}
-
-
-
-
-				/**Vector3d vec3color = new Vector3d(1, yellow, 0);
-				Vector3d vec3colorWater = new Vector3d (50, blue, 100);
-				ParticleColours[i] = vec3colorWater; 
-				ParticleColours[i] = vec3color;**/
-
 			}
 		}
-			
 
-		protected void UpdateVBOs(Color color) {
+
+		protected void UpdateVBOs (Color color)
+		{
 			ParticlePositions = new Vector2d[Particles.Count]; 
 			ParticleColours = new Vector3d[Particles.Count];
 			for (int i = 0; i < Particles.Count; i++) {
-				ParticlePositions[i] = Particles.ElementAt(i).GetPosition();
-				Vector3d vec3color = new Vector3d(color.R, color.G, color.B);
-				ParticleColours[i] = vec3color; //TODO: change accordingly
-			}
-		}
-
-		/**protected override void UpdateVBOs2() {
-			ParticlePositions2 = new Vector2d[Particles2.Count]; 
-			ParticleColours2 = new Vector3d[Particles2.Count];
-			for (int i = 0; i < Particles2.Count; i++) {
-				Panel = (FireUserSettings)GetParticleSystemSettingsPanel();
-				ParticlePositions2[i] = Particles2.ElementAt(i).GetPosition();
-				Color color = Panel.getColor();
-
-				double yellow = 1;
-
-				yellow = (yellow - (( Particles2.ElementAt(i).GetPosition().Y) /100 ));
-
-				Vector3d vec3color = new Vector3d(1, yellow, 0);
-				ParticleColours2[i] = vec3color; 
+				ParticlePositions [i] = Particles.ElementAt (i).GetPosition ();
+				Vector3d vec3color = new Vector3d (color.R, color.G, color.B);
+				ParticleColours [i] = vec3color; //TODO: change accordingly
 			}
 		}
 
 
-		protected void UpdateVBOs2(Color color) {
-			ParticlePositions2 = new Vector2d[Particles2.Count]; 
-			ParticleColours = new Vector3d[Particles2.Count];
-			for (int i = 0; i < Particles2.Count; i++) {
-				ParticlePositions2[i] = Particles2.ElementAt(i).GetPosition();
-				Vector3d vec3color = new Vector3d(color.R, color.G, color.B);
-				ParticleColours[i] = vec3color; //TODO: change accordingly
-			}
-		}
-            **/
 
 
-		protected override void DecrementLifetime() {
-			LifetimeHandler.DecrementLifetime(Particles);
+		protected override void DecrementLifetime ()
+		{
+			LifetimeHandler.DecrementLifetime (Particles);
 		}
 
-		protected override void RemoveExpiredParticles() {
-			ExpirationHandler.handleExpiration(Particles);
+		protected override void RemoveExpiredParticles ()
+		{
+			ExpirationHandler.handleExpiration (Particles);
 		}
 
-		protected override void GenerateNewParticles() {
+		protected override void GenerateNewParticles ()
+		{
 
-			for (int i = 0; i < (ParticleSettings.GetNumberOfNewParticlesPerFrame() * 1.0); i++)
-			{
-				Particles.Add(ParticleGenerator.GenerateParticle());
-				Particles.Add(ParticleGenerator.GenerateParticle2(minX, maxX));
-			}
-		}
-
-		protected override void UpdateParticlePositions() {
-			PositionUpdater.SetContext(Context);
-			PositionUpdater.UpdatePositions(Particles);
-		}
-
-		private void CreateInitialParticles() {
-			for (int i = 0; i < ParticleSettings.GetInitialNumberOfParticles(); i++) {
-				Particles.Add(ParticleGenerator.GenerateParticle());
+			for (int i = 0; i < (ParticleSettings.GetNumberOfNewParticlesPerFrame () * 1.0); i++) {
+				Particles.Add (ParticleGenerator.GenerateParticle ());
 				Particles.Add (ParticleGenerator.GenerateParticle2 (minX, maxX));
 			}
 		}
 
-		public override string GetDescription() {
-			return "Simulating a wall of fire using a particle system";
+		protected override void UpdateParticlePositions ()
+		{
+			PositionUpdater.SetContext (Context);
+			PositionUpdater.UpdatePositions (Particles);
 		}
 
-		public override ParticleSystemSettingsPanel GetParticleSystemSettingsPanel() {
+		private void CreateInitialParticles ()
+		{
+			for (int i = 0; i < ParticleSettings.GetInitialNumberOfParticles (); i++) {
+				Particles.Add (ParticleGenerator.GenerateParticle ());
+				Particles.Add (ParticleGenerator.GenerateParticle2 (minX, maxX));
+			}
+		}
+
+		public override string GetDescription ()
+		{
+			return "Simulating a wall of fire being hit by a waterfall using a particle system";
+		}
+
+		public override ParticleSystemSettingsPanel GetParticleSystemSettingsPanel ()
+		{
 			return Panel;
 		}
 
-		public override ParticleSettings GetParticleSettings() {
-			Panel = (FireUserSettings)GetParticleSystemSettingsPanel();
-			ParticleSettings.WithInitialNumberOfParticles(0);
-			ParticleSettings.WithNewParticlesPerFrame(20);
-			ParticleSettings.WithLifetime(110); 
-			ParticleSettings.WithVelocity(5);
+		public override ParticleSettings GetParticleSettings ()
+		{
+			Panel = (FireUserSettings)GetParticleSystemSettingsPanel ();
+			ParticleSettings.WithInitialNumberOfParticles (0);
+			ParticleSettings.WithNewParticlesPerFrame (20);
+			ParticleSettings.WithLifetime (110); 
+			ParticleSettings.WithVelocity (5);
 			ParticleSettings.WithGlBackgroundColor (Color.Gray);
 			return ParticleSettings;
 		}
