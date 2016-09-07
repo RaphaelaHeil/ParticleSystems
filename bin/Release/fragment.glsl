5a5fd77cca265b8e29a0d8dfde1bfd01b3f8aa1a
RaphaelaHeil <rhei@stud.hs-bremen.de>
2016-09-07 10:32:45 +0200

    pushing release files for demo purposes

---
+++ bin/Release/fragment.glsl
@@ -0,0 +1,10 @@
+﻿#version 120
+
+varying vec3 vColour;
+//out vec4 outputColour;
+
+//Fragment Shader
+void main()
+{
+	gl_FragColor = vec4(vColour, 0.0); //use the previously specified colour without any modification
+}
---
+++ bin/Release/vertex.glsl
@@ -0,0 +1,15 @@
+﻿#version 120
+
+attribute vec2 vertexPosition;
+attribute vec3 vertexColour;
+varying vec3 vColour;
+uniform mat4 mProjection;
+
+// Vertex Shader  
+void main()
+{
+
+gl_Position =  mProjection * vec4(vertexPosition, 0.0, 1.0);
+//gl_Position = vec4(vertexPosition, 0.0, 1.0); //places all vertices in the same plane 
+vColour = vertexColour;
+}
